6db58fdda98963d0ff7034fd41519dbb46c1b2fb
ping602 <k97049@gmail.com>
2022-04-30 23:31:27 +0800

    3

--- r.py
+++ r.py
@@ -18,3 +18,10 @@ for d in data:
 	new.append(d)
 	print('一共有', len(new), '留言長度小於100')
 	print(new[0])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '留言提到good')
+print(good[0])
